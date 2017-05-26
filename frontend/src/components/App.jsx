8bdeb5db6ea8655141dcfb6b4de2cef3afc14a6b
sz-piotr <szlachciak.piotr@gmail.com>
2017-05-26 10:12:46 +0200

    force auth on /test route

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,20 +1,40 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Home from './Home';
 import Login from './Login/Login';
 import Test from './Test/Test';
 import NotFound from './NotFound';
 
-const App = () => (
+function checkAuth(component, loggedIn) {
+  return () => {
+    if(loggedIn) {
+      return component;
+    } else {
+      return <Redirect to='/login'/>
+    }
+  }
+}
+
+let App = (props) => (
   <Router basename="/app">
     <Switch>
       <Redirect exact from='/' to='/home'/>
       <Route exact path="/home" component={Home} />
       <Route exact path="/login" component={Login} />
-      <Route exact path="/test" component={Test} />
+      <Route exact path="/test" render={checkAuth(<Test />, props.loggedIn)} />
       <Route path="/*" component={NotFound} />
     </Switch>
   </Router>
 )
+
+function mapStateToProps(state) {
+  return {
+    loggedIn: state.login.loggedIn,
+  }
+}
+
+App = connect(mapStateToProps)(App)
+
 
 export default App;
