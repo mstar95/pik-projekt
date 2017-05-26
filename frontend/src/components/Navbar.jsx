284687676df6e17a8d8ccc7792354fe795b9c54b
sz-piotr <szlachciak.piotr@gmail.com>
2017-05-26 09:47:42 +0200

    change navbar based on logged in state

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Navbar = (props) => (
-  <nav className="navbar">
-    <ul>
-      <li><NavLink to="/home">Home</NavLink></li>
-      <li><NavLink to="/login">Login</NavLink></li>
-      <li><NavLink to="/test">Test</NavLink></li>
-      <li><NavLink to="/404">404</NavLink></li>
-    </ul>
-  </nav>
-)
+let Navbar = (props) => {
+  if(!props.loggedIn) {
+    return (
+      <nav className="navbar">
+        <ul>
+          <li><NavLink to="/home">Home</NavLink></li>
+          <li><NavLink to="/login">Login</NavLink></li>
+        </ul>
+      </nav>
+    )
+  } else {
+    return (
+      <nav className="navbar">
+        <ul>
+          <li><NavLink to="/home">Home</NavLink></li>
+          <li><NavLink to="/test">Test</NavLink></li>
+          <li><NavLink to="/logout">Logout</NavLink></li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    loggedIn: state.login.loggedIn,
+  }
+}
+
+Navbar = connect(mapStateToProps)(Navbar)
 
 export default Navbar;
