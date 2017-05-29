173c9e2db980d44eff91fe381849ab8092924469
sz-piotr <szlachciak.piotr@gmail.com>
2017-05-29 17:11:20 +0200

    fix: 404 screen

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -3,7 +3,10 @@ import PageWrapper from './PageWrapper';
 
 const NotFound = () => (
   <PageWrapper>
-    {'404 :('}
+    <div className='page-item'>
+      <h1 className='page-info'>{'Not found'}</h1>
+      <p>{'404 :('}</p>
+    </div>
   </PageWrapper>
 )
 
