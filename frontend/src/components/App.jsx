aacc51332ba5633c6c0bdf0b934ed0beae6e1ec7
sz-piotr <szlachciak.piotr@gmail.com>
2017-06-01 19:08:31 +0200

    reset app.jsx

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -4,69 +4,6 @@ import { connect } from 'react-redux';
 import Home from './Home';
 import Login from './Login/Login';
 import Logout from './Login/Logout';
-import React from 'react';
-import axios from 'axios';
-import PageWrapper from '../PageWrapper';
-import { NavLink } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { testListFetchStart, testListFetchSuccess, testListFetchFail } from '../../actions';
-
-class TestList extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.getTests();
-  }
-
-  getTests() {
-    let { dispatch } = this.props;
-
-    dispatch(testListFetchStart());
-    return axios.get('/api/tests')
-      .catch(error => dispatch(testListFetchFail()))
-      .then(response => dispatch(testListFetchSuccess(response.data)));
-  }
-
-  render() {
-    let { loading, error, tests } = this.props;
-
-    let content;
-    if (loading) {
-      content = <p>{'Loading...'}</p>
-    } else if (error) {
-      content = <p>{'Error'}</p>
-    } else {
-      let testListItems = tests.map(test => (
-        <li key={test.id}><NavLink to={'/test/' + test.id}>{test.title}</NavLink></li>
-      ));
-      content = <ul className='link-list'>{testListItems}</ul>
-    }
-
-    return (
-      <PageWrapper>
-        <div className='page-item'>
-          <h1 className='page-info'>{'Tests'}</h1>
-          {content}
-        </div>
-      </PageWrapper>
-    )
-  }
-}
-
-function mapStateToProps(state) {
-  return {
-    loading: state.testlist.loading,
-    error: state.testlist.error,
-    tests: state.testlist.tests
-  }
-}
-
-TestList = connect(mapStateToProps)(TestList)
-
-export default TestList;
-
-import Admin from './Admin/ResultList';
-import Users from './Admin/Users';
 import Test from './Test/Test';
 import TestList from './Test/TestList';
 import NotFound from './NotFound';
@@ -76,10 +13,9 @@ let App = (props) => (
     <Switch>
       <Redirect exact from='/' to='/home'/>
       <Route exact path="/home" component={Home} />
-      <Route exact path="/admin" component={Admin} />
-      <Route exact path="/users" component={Users} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/logout" component={Logout} />
+      <Route exact path="/users" component={Users} />
       <Route exact path="/tests" component={TestList} />
       <Route exact path="/test/:id" component={Test} />
       <Route path="/*" component={NotFound} />
