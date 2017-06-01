e9cab6425d0abedb417d3cfb15d4c71133088ef8
Michal <m.star@wp.pl>
2017-06-01 17:58:16 +0200

    100% of logout

--- backend/src/main/kotlin/app/security/WebSecurityConfig.kt
+++ backend/src/main/kotlin/app/security/WebSecurityConfig.kt
@@ -2,11 +2,14 @@ package app.security
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler
 
 @Configuration
 @EnableWebSecurity
@@ -36,6 +39,8 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
                 .failureHandler(failureHandler)
                 .and()
                 .logout().logoutUrl("/api/logout")
+                .logoutSuccessHandler( HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK))
+
 
     }
 
